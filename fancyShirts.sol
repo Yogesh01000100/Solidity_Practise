9dbeb64b973db56d8462348f1c75cd92d5fa9ae3
Yogesh01000100 <yogeshone678@gmail.com>
2023-09-01 20:08:41 +0530

    solidity fundamentals| new add

---
+++ fancyShirts.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.22 <=0.8.19;
+
+contract fancyShirt {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    enum Size {
+        Small,
+        Medium,
+        Large
+    }
+
+    enum Color {
+        Red,
+        Green,
+        Blue
+    }
+
+    struct userChoice {
+        Size _size;
+        Color _color;
+    }
+    mapping(address => userChoice) closet;
+
+    modifier correctAmount(Size size, Color color) {
+        require(
+            getShirtPrice(size, color) == msg.value,
+            "Incorrect Amount sent!"
+        );
+        _;
+    }
+
+    function getShirtPrice(Size size, Color color) public pure returns (uint) {
+        uint price;
+        if (size == Size.Small) {
+            price += 10;
+        } else if (size == Size.Medium) {
+            price += 15;
+        } else {
+            price += 20;
+        }
+
+        if (color != Color.Red) {
+            price += 5;
+        }
+        return price;
+    }
+
+    function buyShirt(
+        Size size,
+        Color color
+    ) public payable correctAmount(size, color) {
+        closet[msg.sender]._size = size;
+        closet[msg.sender]._color = color;
+    }
+
+    function getCloset(address addr) public view returns (userChoice memory) {
+        return closet[addr];
+    }
+}
